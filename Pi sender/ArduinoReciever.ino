0e961b2ba408bf1171dc3ff798f255ab99d444b2
kartik khandelwal <khandelwal.kartik4@gmail.com>
2017-04-13 19:01:47 +0000

    Add files via upload
    Arduino is Receiving data from Python script running on  Raspberry pi and sending back acknowledge data to Raspberry pi

---
+++ Pi sender/ArduinoReciever.ino
@@ -0,0 +1,44 @@
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+#include "printf.h"
+
+
+RF24 radio(9,10);
+
+
+void setup(void){
+  while(!Serial);
+  Serial.begin(9600);
+  radio.begin();
+  radio.setPALevel(RF24_PA_MAX);
+  radio.setChannel(0x76);
+  radio.openWritingPipe(0xF0F0F0F0E1LL);
+  const uint64_t pipe = 0xE8E8F0F0E1LL;
+  radio.openReadingPipe(1,pipe);
+  radio.enableDynamicPayloads();
+  radio.powerUp(); 
+}
+
+void loop(void){
+   radio.startListening();
+   Serial.println("Starting loop.Radio on.");
+   char recievedMessage[32] = {0};
+   if(radio.available()){
+    radio.read(recievedMessage , sizeof(recievedMessage));
+    Serial.println(recievedMessage);
+    Serial.println("Turning off th Radio.");
+    radio.stopListening(); 
+    String stringMessage(recievedMessage);
+
+    if(stringMessage == "11"){
+     Serial.println("look like they want a string ");
+     const char text[] = "HELLO World!";  
+     radio.write(text , sizeof(text));
+       Serial.println("We sent our message");
+    }
+    
+   }
+   delay(200);
+}
+
